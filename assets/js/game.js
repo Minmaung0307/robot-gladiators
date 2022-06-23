ebe5fafd584f93ec3be09d3716544d21e979d2b5
Min Maung <minmaung0307@gmail.com>
2022-06-23 17:00:02 -0400

    create and execute functions, interact with player, and store data

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,36 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+// window.prompt("What is your robot's name?");
+// window.alert("This is an alert! JavaScript is running!");
+
+// Note the lack of quotation marks around playerName
+//window.alert(playerName);
+
+// What is this?
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+/*
+var playerName = "Min";
+
+// Min the Robot is ready for battle!
+console.log("Min" + " is ready for battle!");
+
+// "Min is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Min, has won!
+console.log("Your robot, " + playerName + ", has won!");
+--*/
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+ // fight();
